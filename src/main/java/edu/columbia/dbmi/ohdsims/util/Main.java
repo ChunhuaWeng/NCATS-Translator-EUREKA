b24a9d63223f0ce3b3719de700fea21631506f06
Wei Wei <weiwei@Wei-MacBook-Air.local>
2017-10-03 00:11:02 -0400

    question preprocessing 2, 10/02/2017

---
+++ src/main/java/edu/columbia/dbmi/ohdsims/util/Main.java
@@ -0,0 +1,19 @@
+package edu.columbia.dbmi.ohdsi;
+import java.io.IOException;
+import java.io.*;
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        Question question = new Question();
+        question.getQuestion();
+        question.getTemplates();
+        question.analyzeQuestion();
+        question.getConceptID();
+        System.out.println(question.keyConcepts);
+    }
+}
